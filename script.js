979b6b0ef7f7cb50ab1be3ad78fd928d052fe5e2
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2025-12-31 21:04:16 +0500

    call playGame functoin and writes its definition

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ Choices.forEach((choice)=>{
    // console.log(choice);
    const UserChoice = choice.getAttribute("id");
    choice.addEventListener("click",() => {
-    console.log("choice are clicked and its ID is :",UserChoice);
+    //console.log("choice are clicked and its ID is :",UserChoice);
+    playGame(UserChoice);
    })
 })
 
+
+const playGame = (choice) => {
+console.log("User choice is : ", choice);
+}
